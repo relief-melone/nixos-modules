fe575708957ae11fe4d50d5dac33937692b34547
Relief Melone <relief.melone@gmail.com>
2025-04-10 14:58:54 +0200

    added inputs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,20 @@
 {
   description = "Reusable modules for NixOS configurations";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
   outputs = { self, ... }:{
     nixosModules.hm-nixvim = import ./home-manager/nixvim;
   };
