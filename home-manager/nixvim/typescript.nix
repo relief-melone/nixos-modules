4bca6b05dd1d8e33415abd53ffc142913841ea6b
Relief Melone <relief.melone@gmail.com>
2025-04-28 11:56:19 +0200

    disabled typescript debugging for now

--- home-manager/nixvim/typescript.nix
+++ home-manager/nixvim/typescript.nix
@@ -40,50 +40,50 @@
       nvim-dap
     ];
 
-    extraConfigLua = ''
-      local dap_vscode_js = require("dap-vscode-js")
-      local languages = { "typescript", "javascript", "vue" }
-
-      dap_vscode_js.setup({
-        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }
-      })
-
-      -- DEBUG CONFIG TYPESCRIPT
-      for _, lang in ipairs(languages) do
-        dap.configurations[lang] = {
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Launch file",
-            program = "dist/''${fileBasenameNoExtension}.js",
-            cwd = "''${workspaceFolder}",
-          },
-          {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach to process",
-            processId = require'dap.utils'.pick_process,
-            cwd = "''${workspaceFolder}",
-          },
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Run Application",
-            program = "dist/index.js",
-            cwd = "''${workspaceFolder}",
-          },
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Run npm test",
-            program = "node_modules/mocha/bin/_mocha",
-            cwd = "''${workspaceFolder}",
-          }
-        }
-      end
-      -- DEBUG CONFIG JAVASCRIPT
-    '';
-
+#    extraConfigLua = ''
+#      local dap_vscode_js = require("dap-vscode-js")
+#      local languages = { "typescript", "javascript", "vue" }
+#
+#      dap_vscode_js.setup({
+#        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }
+#      })
+#
+#      -- DEBUG CONFIG TYPESCRIPT
+#      for _, lang in ipairs(languages) do
+#        dap.configurations[lang] = {
+#          {
+#            type = "pwa-node",
+#            request = "launch",
+#            name = "Launch file",
+#            program = "dist/''${fileBasenameNoExtension}.js",
+#            cwd = "''${workspaceFolder}",
+#          },
+#          {
+#            type = "pwa-node",
+#            request = "attach",
+#            name = "Attach to process",
+#            processId = require'dap.utils'.pick_process,
+#            cwd = "''${workspaceFolder}",
+#          },
+#          {
+#            type = "pwa-node",
+#            request = "launch",
+#            name = "Run Application",
+#            program = "dist/index.js",
+#            cwd = "''${workspaceFolder}",
+#          },
+#          {
+#            type = "pwa-node",
+#            request = "launch",
+#            name = "Run npm test",
+#            program = "node_modules/mocha/bin/_mocha",
+#            cwd = "''${workspaceFolder}",
+#          }
+#        }
+#      end
+#      -- DEBUG CONFIG JAVASCRIPT
+#    '';
+#
     autoCmd = [{
       event = [ "BufWritePre" ] ;
       pattern = [ "*.tsx" "*.ts" "*.jsx" "*.js" ];
