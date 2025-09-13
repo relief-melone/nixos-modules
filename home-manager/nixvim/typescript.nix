d875fd344231cdf57590f1e913c9258c2040ac01
Relief Melone <relief.melone@gmail.com>
2025-09-13 16:10:52 +0200

    initial commit. Added extraConfigLua to typescript

--- home-manager/nixvim/typescript.nix
+++ home-manager/nixvim/typescript.nix
@@ -30,5 +30,25 @@
       pattern = [ "*.tsx" "*.ts" "*.jsx" "*.js" ];
       command = "silent! EslintFixAll";
     }];
+
+    # Vue Support
+    extraConfigLua = ''
+      require('lspconfig').ts_ls.setup{
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+              languages = { "javascript", "typescript", "vue" }
+            },
+          },
+        },
+        filetypes = {
+          "javascript",
+          "typescript",
+          "vue",
+        },
+      }
+    '';
   };
 }
