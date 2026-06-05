0e670c10a4e50b357b387bd7e75270f5ccccca06
Relief Melone <relief.melone@gmail.com>
2026-06-05 12:27:14 +0200

    addind plugin

--- home-manager/nixvim/default.nix
+++ home-manager/nixvim/default.nix
@@ -54,12 +54,16 @@
       vim-flog
       vim-helm
       vim-gitbranch
+      tint-nvim
       #vim-airline-themes
     ];
 
     extraConfigLua = ''
       -- APPEARANCE
       vim.api.nvim_set_hl(0, 'LineNr', { fg = "#35b6e6" })
+      --   Line Separator
+      vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#ff007c', bold = true })
+
 
       --   Cursor style
       vim.o.guicursor = 'n-v-c-sm:block,i:ver100-iCursor-blinkon1-blinkwait10'
